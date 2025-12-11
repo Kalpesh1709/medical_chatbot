df677bcb95c43774ac437fbd5bbd3692104bfc88
kalpesh thakare <thakare.kalpesh17@gmail.com>
2025-12-12 04:34:37 +0530

    hii

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # medical_chatbot
+
+#create virtual env 
+
+py -3.11 -m venv test
+./test/scripts/activate
+
+#installrequirements
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI  Medical chatbot Project',
+    version= '0.0.0',
+    author= 'Kalpesh Thakare',
+    author_email= 'thakare.kalpesh17@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
